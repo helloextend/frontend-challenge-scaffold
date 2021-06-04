95c4f89ebd6d78e1aee41cc7692503569f0e768d
Matthew Schrepel <matthew@extend.com>
2021-06-03 19:05:31 -0700

    add instructions, payload return example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 const IMAGES_URL = 'https://dog.ceo/api/breed/hound/images/random/55'
+/**
+ * 
+ * @returns { "message": ["https://images.dog.ceo/breeds/hound-afghan/n02088094_1126.jpg", ...] }
+ * 
+ * Step 1: Render all the images returned
+ * Step 2: Create chunks of 10 images from the returned list
+ * Step 3: Render the first 10 of the list
+ * Step 4: Build a way to increment to the next 10
+ * Step 5: Build a way to decrement to the previous 10
+ * Step 6: Create a UI that shows which page you're viewing
+ * 
+ */
 
 function App() {
   return (
