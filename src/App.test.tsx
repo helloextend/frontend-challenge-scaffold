29595ccadd2e572a29602c8c9ad16a8885250d52
Jesse Heaslip <jesse.heaslip@gmail.com>
2021-09-17 11:48:00 -0500

    PR, feedback

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 test("a user interaction", () => {
+  // Write a test that covers a user interaction
   const wrapper = render(<App />);
 });
